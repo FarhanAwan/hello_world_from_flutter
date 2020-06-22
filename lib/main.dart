de216e475aa72cc2f5e9cc35419fdefd8afe9ce3
Farhan Awan <farhankhanawan@gmail.com>
2020-06-22 20:47:01 +0500

    Hello world from flutter
    testing flutter development process

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter'),
+//         ),
+//         body: Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome To Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Farhan Awan"),
+        ),
+        body: Center(
+          child: Text("Hello From Farhan Awan"),
+        ),
+      ),
+      
+    );
+  }
+}
+
+
+
+
